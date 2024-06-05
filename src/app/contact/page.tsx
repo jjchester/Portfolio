44604862f56bd91d3441b0c1f15c903ad90f60d3
Justin <justinjchester@icloud.com>
2024-06-05 19:39:12 -0400

    Finish contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,91 @@
+'use client';
+import { Box, Button, HStack, Heading, Input, Select, Stack, Text, Textarea, VStack } from "@chakra-ui/react";
+import { useForm, ValidationError } from "@formspree/react";
+import { useState } from 'react';
+
+export default function Contact() {
+  const [state, handleSubmit] = useForm(process.env.FORMSPREE_KEY!);
+  const [error, setError] = useState(false);
+  const [formData, setFormData] = useState({
+    email: '',
+    message: '',
+    inquiryType: 'general'
+  });
+
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const customHandleSubmit = async (e: any) => {
+    e.preventDefault();
+    if (formData.email === '' || formData.message === '' || formData.inquiryType === '') {
+      setError(true);
+      return;
+    }
+    await handleSubmit({
+      ...formData
+    });
+  };
+
+  return (
+    <Box maxW={"80%"} mx={"auto"} p={4}>
+      <form onSubmit={customHandleSubmit}>
+        <Stack direction={"column"} spacing={2}>
+          <Text fontSize={"3xl"}>Get in touch</Text>
+          <Select
+            id="inquiryType"
+            name="inquiryType"
+            maxW={"200px"}
+            value={formData.inquiryType}
+            onChange={handleChange}
+          >
+            <option value="general">General Inquiry</option>
+            <option value="support">Recruiter</option>
+          </Select>
+          <Input
+            id="email"
+            type="email"
+            name="email"
+            maxW={"250px"}
+            placeholder="Enter your email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <ValidationError
+            prefix="Email"
+            field="email"
+            errors={state.errors}
+          />
+          <Textarea
+            id="message"
+            name="message"
+            placeholder="Your message here"
+            value={formData.message}
+            onChange={handleChange}
+          />
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+          {(state.errors || error) && <Text color="red.500">There was an error submitting your message.</Text>}
+          {state.succeeded && <p>Your message has been sent. Thanks for reaching out!</p>}
+          <Button
+            mx={"auto"}
+            w="s"
+            colorScheme="codegreen.400"
+            variant={'outline'}
+            type="submit"
+            isDisabled={state.submitting || state.succeeded}
+          >
+            Submit
+          </Button>
+        </Stack>
+      </form>
+    </Box>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,9 +26,13 @@ export default function Home() {
     animate: { x: 0, y: 0, opacity: 1 },
     transition: { duration: 1, ease: "easeInOut" },
   };
+  const itemVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0 }
+  };
   return (
     <>
-      {/* <Card animate={cardAnimation}>
+      <Card animate={cardAnimation}>
         <SimpleGrid columns={5} spacing={0}>
           <MotionGridItem
             colSpan={{ base: 5, md: 3 }}
@@ -69,115 +73,34 @@ export default function Home() {
             </Text>
           </MotionGridItem>
         </SimpleGrid>
-      </Card> */}
+      </Card>
       <Box h={{ base: "50px", md: "100px" }} />
       <Text fontSize={"5xl"} fontWeight={"600"} textAlign={"center"} pb={4}>Skills</Text>
       <Box display="flex" justifyContent="center" w="100%" pb={8}>
         <SimpleGrid minChildWidth="240px" spacing={4} w="100%" mx={'auto'}>
-          <GridItem>
-            <SkillCard
-              maxW="300px"
-              image={<SwiftIcon width="24px" height="24px" color="codegreen.400" />}
-              title={"iOS"}
-              description={"Using Swift, SwiftUI, UIKit, Cocoa, and core libraries to create native iOS apps."}
-            />
-          </GridItem>
-          <GridItem>
-            <SkillCard
-              maxW="300px"
-              image={<GitIcon width="24px" height="24px" color="codegreen.400" />}
-              title={"Git"}
-              description={"Working with GitHub and GitLab for version control, CI/CD, and code reviews."}
-            />
-          </GridItem>
-          <GridItem>
-            <SkillCard
-              maxW="300px"
-              image={<ApiIcon width="24px" height="24px" color="codegreen.400" />}
-              title={"API Building"}
-              description={"Creating and testing APIs with tools like Node, Symfony, and Postman."}
-            />
-          </GridItem>
-          <GridItem>
-            <SkillCard
-              maxW="300px"
-              image={<ReactLogoIcon width="24px" height="24px" color="codegreen.400" />}
-              title={"React"}
-              description={"Building web applications using React and NextJS 14 with common libraries"}
-            />
-          </GridItem>
-          <GridItem>
-            <SkillCard
-              maxW="300px"
-              image={<FirebaseIcon width="24px" height="24px" color="codegreen.400" />}
-              title={"Firebase"}
-              description={"Using Firebase auth, Firestore NoSQL database, and cloud functions."}
-            />
-          </GridItem>
-          <GridItem>
-            <SkillCard
-              maxW="300px"
-              image={<MysqlIcon width="24px" height="24px" color="codegreen.400" />}
-              title={"MySQL"}
-              description={"Writing MySQL queries & migrations directly or using ORM tools like Prisma."}
-            />
-          </GridItem>
-          <GridItem>
-            <SkillCard
-              maxW="300px"
-              image={<DockerIcon width="24px" height="24px" color="codegreen.400" />}
-              title={"Docker"}
-              description={"Creating and managing containers with Docker and Docker Compose."}
-            />
-          </GridItem>
-          <GridItem>
-            <SkillCard
-              maxW="300px"
-              image={<JiraIcon width="24px" height="24px" color="codegreen.400" />}
-              title={"Jira"}
-              description={"Using Jira to manage and track tasks, epics, sprints, and roadmaps."}
-            />
-          </GridItem>
-          <GridItem>
-            <SkillCard
-              maxW="300px"
-              image={<ConfluenceIcon width="24px" height="24px" color="codegreen.400" />}
-              title={"Confluence"}
-              description={"Documenting new and existing features using Confluence pages."}
-            />
-          </GridItem>
-          <GridItem>
-            <SkillCard
-              maxW="300px"
-              image={<UxIcon width="24px" height="24px" color="codegreen.400" />}
-              title={"UX/UI"}
-              description={"Designing clean and user-friendly interfaces with tools like Figma and XD."}
-            />
-          </GridItem>
-          <GridItem>
-            <SkillCard
-              maxW="300px"
-              image={<BashIcon width="24px" height="24px" color="codegreen.400" />}
-              title={"Scripting"}
-              description={"Automating repetitive tasks & commands with scripts using ZSH and Bash."}
-            />
-          </GridItem>
-          <GridItem>
-            <SkillCard
-              maxW="300px"
-              image={<BugIcon width="24px" height="24px" color="codegreen.400" />}
-              title={"Debugging"}
-              description={"Troubleshooting with tools like LLDB, Postman, breakpoints and logging."}
-            />
-          </GridItem>
-          {/* <GridItem>
-            <SkillCard
-              maxW="300px"
-              image={<KubernetesIcon width="24px" height="24px" color="codegreen.400" />}
-              title={"Kubernetes"}
-              description={"Using Kubernetes, kubens, and kubectl to ."}
-            />
-          </GridItem> */}
+          {[
+            { icon: SwiftIcon, title: "iOS", description: "Using Swift, SwiftUI, UIKit, Cocoa, and core libraries to create native iOS apps." },
+            { icon: GitIcon, title: "Git", description: "Working with GitHub and GitLab for version control, CI/CD, and code reviews." },
+            { icon: ApiIcon, title: "API Building", description: "Creating and testing APIs with tools like Node, Symfony, and Postman." },
+            { icon: ReactLogoIcon, title: "React", description: "Building web applications using React and NextJS 14 with common libraries." },
+            { icon: FirebaseIcon, title: "Firebase", description: "Using Firebase auth, Firestore NoSQL database, and cloud functions." },
+            { icon: MysqlIcon, title: "MySQL", description: "Writing MySQL queries & migrations directly or using ORM tools like Prisma." },
+            { icon: DockerIcon, title: "Docker", description: "Creating and managing containers with Docker and Docker Compose." },
+            { icon: JiraIcon, title: "Jira", description: "Using Jira to manage and track tasks, epics, sprints, and roadmaps." },
+            { icon: ConfluenceIcon, title: "Confluence", description: "Documenting new and existing features using Confluence pages." },
+            { icon: UxIcon, title: "UX/UI", description: "Designing clean and user-friendly interfaces with tools like Figma and XD." },
+            { icon: BashIcon, title: "Scripting", description: "Automating repetitive tasks & commands with scripts using zsh and Bash." },
+            { icon: BugIcon, title: "Debugging", description: "Troubleshooting with tools like LLDB, Postman, breakpoints and logging." }
+          ].map((skill, index) => (
+            <MotionGridItem key={index} as={motion.div} variants={itemVariants} initial="hidden" animate="visible" transition={{ duration: 1, ease: "easeInOut" }}>
+              <SkillCard
+                maxW="300px"
+                image={<skill.icon width="24px" height="24px" color="codegreen.400" />}
+                title={skill.title}
+                description={skill.description}
+              />
+            </MotionGridItem>
+          ))}
         </SimpleGrid>
       </Box>
     </>
